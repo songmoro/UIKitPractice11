2157c068027fd89828469bd50701aa2fc8d51b0d
songmoro <wlqgkr@naver.com>
2025-07-11 15:57:36 +0900

    369 계산 구현

--- UIKitPractice11/VC/Game369ViewController.swift
+++ UIKitPractice11/VC/Game369ViewController.swift
@@ -26,5 +26,28 @@ class Game369ViewController: ViewController {
     
     @IBAction func inputTextFieldEditingDidEnd(_ sender: UITextField) {
         print(#function)
+        print(calculate(sender.text))
+    }
+    
+    func calculate(_ text: String?) -> (Int?, [String]) {
+        guard let text = text else { return (nil, []) }
+        guard let number = Int(text) else { return (nil, []) }
+        
+        return (1...number).reduce(into: (0, [String]())) { partialResult, i in
+            var str = String(i)
+            
+            if str.contains(where: { "369".contains($0) }) {
+                str = str.map {
+                    if "369".contains($0) {
+                        partialResult.0 += 1
+                        return "👏"
+                    }
+                    return "\($0)"
+                }
+                .joined()
+            }
+            
+            partialResult.1.append(str)
+        }
     }
 }
