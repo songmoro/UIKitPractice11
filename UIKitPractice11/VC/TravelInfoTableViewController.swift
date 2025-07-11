7ac233cef867f5418711a50325ca23ebc9e47f07
songmoro <wlqgkr@naver.com>
2025-07-11 19:22:04 +0900

    각 탭 네비게이션 및 탭 설정, 도시 상세 정보 탭 추가

---
+++ UIKitPractice11/VC/TravelInfoTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TravelInfoTableViewController.swift
+//  UIKitPractice11
+//
+//  Created by 송재훈 on 7/11/25.
+//
+
+import UIKit
+
+class TravelInfoTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        print(#file, #function)
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+}
