4719fd3bd9fbff86d7795039157a546e204e8943
songmoro <wlqgkr@naver.com>
2025-07-11 22:48:58 +0900

    travelinfo cell 구현

--- UIKitPractice11/Cell/TravelInfoTableViewCell.swift
+++ UIKitPractice11/Cell/TravelInfoTableViewCell.swift
@@ -8,7 +8,58 @@
 import UIKit
 
 class TravelInfoTableViewCell: UITableViewCell {
+    static let identifier = "travelInfoTableViewCell"
+    
+    struct Input {
+        let titleText: String
+        let descriptionText: String?
+        var grade: CGFloat?
+        var save: Int?
+        var like: Bool?
+    }
+    
+    @IBOutlet var title: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var stars: [UIImageView]!
+    @IBOutlet var gradeAndSaveLabel: UILabel!
+    @IBOutlet var travel_image: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    var travel: Input? {
+        didSet {
+            updateLabels()
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        travel_image.layer.cornerRadius = 12
+    }
+    
+    override func prepareForReuse() {
+        travel = nil
+    }
+    
+    func updateLabels() {
+        guard let travel else { return }
+        title.text = travel.titleText
+        if let descriptionText = travel.descriptionText {
+            descriptionLabel.text = descriptionText
+        }
+        
+        guard let grade = travel.grade else { return }
+        var gradeAndSaveText = "(\(grade))"
+        updateStars(to: Int(grade))
+        
+        if let save = travel.save {
+            gradeAndSaveText.append("∙저장 \(save.formatted(.number))")
+        }
+        
+        gradeAndSaveLabel.text = gradeAndSaveText
+    }
+    
+    func updateStars(to: Int) {
+        for i in 0...(stars.count - 1) {
+            stars[i].tintColor = i < to ? .systemYellow : .systemGray
+        }
     }
 }
--- UIKitPractice11/VC/TravelInfoTableViewController.swift
+++ UIKitPractice11/VC/TravelInfoTableViewController.swift
@@ -6,16 +6,50 @@
 //
 
 import UIKit
+import Kingfisher
 
 class TravelInfoTableViewController: UITableViewController {
     let travelInfo = TravelInfo()
     
+    // TODO: 메모리 절약
+    
     override func viewDidLoad() {
         print(#file, #function)
         super.viewDidLoad()
+        
+        tableView.rowHeight = 150
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        travelInfo.travel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: TravelInfoTableViewCell.identifier,
+            for: indexPath
+        ) as? TravelInfoTableViewCell else { return UITableViewCell() }
+        
+
+        let travel = travelInfo.travel[indexPath.row]
+        cell.travel = TravelInfoTableViewCell.Input(
+            titleText: travel.title,
+            descriptionText: travel.description,
+            grade: travel.grade,
+            save: travel.save,
+            like: travel.like
+        )
+        
+        if let url = URL(string: travel.travel_image ?? "") {
+            cell.travel_image.kf.setImage(with: url)
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? TravelInfoTableViewCell else { return }
+        guard cell.travel != nil else { return }
+        cell.travel!.grade = CGFloat(Int.random(in: 0...5))
     }
 }
