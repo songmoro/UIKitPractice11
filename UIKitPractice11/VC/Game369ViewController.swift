ece30ceb0b39245e1810402f669f63c7be9f094d
songmoro <wlqgkr@naver.com>
2025-07-11 15:12:47 +0900

    369 스토리보드/VC 생성 및 메인 스토리보드에서 연결

---
+++ UIKitPractice11/VC/Game369ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  Game369ViewController.swift
+//  UIKitPractice11
+//
+//  Created by 송재훈 on 7/11/25.
+//
+
+import UIKit
+
+class Game369ViewController: ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
