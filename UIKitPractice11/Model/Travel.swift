dab539ae21d16b337b85a6b80bbc26b5a57a67f9
songmoro <wlqgkr@naver.com>
2025-07-11 19:13:33 +0900

    Travel 모델 추가

---
+++ UIKitPractice11/Model/Travel.swift
@@ -0,0 +1,18 @@
+//
+//  Travel.swift
+//  UIKitPractice11
+//
+//  Created by 송재훈 on 7/11/25.
+//
+
+import Foundation
+
+struct Travel {
+    let title: String
+    let description: String?
+    let travel_image: String?
+    let grade: CGFloat?
+    let save: Int?
+    var like: Bool?
+    let ad: Bool?
+}
