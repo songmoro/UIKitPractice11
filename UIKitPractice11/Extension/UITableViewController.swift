98e37709b3cc6ee5c5d82c3c7956aa3019fa76c4
songmoro <wlqgkr@naver.com>
2025-07-16 00:46:17 +0900

    작업 완료 후 리로드 데이터 수행 함수 추가

---
+++ UIKitPractice11/Extension/UITableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewController.swift
+//  UIKitPractice11
+//
+//  Created by 송재훈 on 7/16/25.
+//
+
+import UIKit
+
+extension UITableViewController {
+    func willChangeData(_ closure: @escaping () -> (Void)) {
+        closure()
+        tableView.reloadData()
+    }
+}
