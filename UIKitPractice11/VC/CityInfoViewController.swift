3b15a2c9653d221de36f20c129fc3a508bff7518
songmoro <wlqgkr@naver.com>
2025-07-16 13:50:06 +0900

    인기 도시 기본 디자인

--- UIKitPractice11/VC/CityInfoViewController.swift
+++ UIKitPractice11/VC/CityInfoViewController.swift
@@ -7,23 +7,25 @@
 
 import UIKit
 
-class CityInfoViewController: UIViewController {
-
+class CityInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
 }
