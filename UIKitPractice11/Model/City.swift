38d697662434ba42655c71307aee7c4166e840f8
songmoro <wlqgkr@naver.com>
2025-07-16 13:31:32 +0900

    add City Data

---
+++ UIKitPractice11/Model/City.swift
@@ -0,0 +1,14 @@
+//
+//  City.swift
+//  UIKitPractice11
+//
+//  Created by 송재훈 on 7/16/25.
+//
+
+struct City {
+    let city_name: String
+    let city_english_name: String
+    let city_explain: String
+    let city_image: String
+    let domestic_travel: Bool
+}
