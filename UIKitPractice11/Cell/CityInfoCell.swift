1c7d263fc3f9aa49c0f17f1b9d7767d5dd003720
songmoro <wlqgkr@naver.com>
2025-07-16 14:45:53 +0900

    CityInfoCell 디자인, 설정, 데이터 연결

---
+++ UIKitPractice11/Cell/CityInfoCell.swift
@@ -0,0 +1,81 @@
+//
+//  CityInfoCell.swift
+//  UIKitPractice11
+//
+//  Created by 송재훈 on 7/16/25.
+//
+
+import UIKit
+import Kingfisher
+
+extension CityInfoCell {
+    typealias Input = City
+    
+    struct Model {
+        let name: String
+        let englishName: String
+        let imageURL: String
+        let explain: String
+    }
+    
+    func put(_ input: Input) {
+        model = Model(
+            name: input.city_name,
+            englishName: input.city_english_name,
+            imageURL: input.city_image,
+            explain: input.city_explain
+        )
+    }
+}
+
+class CityInfoCell: CustomCell {
+//    let city_name: String
+//    let city_english_name: String
+//    let city_explain: String
+//    let city_image: String
+//    let domestic_travel: Bool
+    
+    @IBOutlet var cityImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var explainLabel: UILabel!
+    
+    var model: Model? {
+        didSet {
+            updateLabels()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        model = nil
+    }
+    
+    func initial() {
+        resetLabels(titleLabel, explainLabel)
+        resetImages(cityImage)
+    }
+    
+    func updateLabels() {
+        guard let model else { return }
+        updateTitle(model.name, model.englishName)
+        updateImage(model.imageURL)
+        updateExplain(model.explain)
+    }
+    
+    func updateTitle(_ name: String, _ englishName: String) {
+        titleLabel.text = "\(name) | \(englishName)"
+    }
+    
+    func updateImage(_ url: String) {
+        guard let url = URL(string: url) else { return }
+        cityImage.kf.setImage(with: url, placeholder: UIImage(systemName: "arrow.circlepath"))
+    }
+    
+    func updateExplain(_ text: String) {
+        explainLabel.text = text
+    }
+}
--- UIKitPractice11/VC/CityInfoViewController.swift
+++ UIKitPractice11/VC/CityInfoViewController.swift
@@ -8,24 +8,34 @@
 import UIKit
 
 class CityInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let cityInfo = CityInfo()
+    
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var segmentedControl: UISegmentedControl!
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
     }
     
     func configureTableView() {
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = 150
+        tableView.separatorStyle = .none
+        tableView.register(of: CityInfoCell.self)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        cityInfo.city.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        let cell = tableView.dequeueCustomCell(of: CityInfoCell.self, for: indexPath)
+        let city = cityInfo.city[indexPath.row]
+        cell?.put(city)
+        
+        return cell ?? UITableViewCell()
     }
 }
